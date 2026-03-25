2e71b404bc73d39658c007e8046e2c9dfb5dd84e
matthew-sabel <sabel.matt@gmail.com>
2026-03-25 11:26:04 -0600

    improved game now

--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
 let duck;
 let score = 0;
 let shots = 0;
+let misses = 0;
+let escaped = 0;
+
 let gameTime = 60 * 30; // 30 seconds at 60 FPS
 let gameOver = false;
+
+let hitMessageTimer = 0;
 
 function setup() {
   createCanvas(900, 500);
@@ -17,6 +22,7 @@ function draw() {
     updateDuck();
     drawDuck();
     drawHUD();
+    drawHitMessage();
 
     gameTime--;
 
@@ -50,8 +56,8 @@ function resetDuck() {
     y: random(80, height - 150),
     w: 50,
     h: 30,
-    speedX: random(3, 5),
-    speedY: random(-1.2, 1.2),
+    speedX: random(4, 6),
+    speedY: random(-1.5, 1.5),
     wingOffset: 0
   };
 }
@@ -66,7 +72,12 @@ function updateDuck() {
   }
 
   if (duck.x > width + 40) {
+    escaped++;
     resetDuck();
+  }
+
+  if (hitMessageTimer > 0) {
+    hitMessageTimer--;
   }
 }
 
@@ -75,18 +86,24 @@ function drawDuck() {
   translate(duck.x, duck.y);
 
   noStroke();
+
+  // body
   fill(90, 60, 20);
   ellipse(0, 0, duck.w, duck.h);
 
+  // head
   fill(70, 40, 10);
   ellipse(-18, -12, 22, 16);
 
+  // beak
   fill(255, 220, 0);
   triangle(-28, -12, -40, -8, -28, -4);
 
+  // wing
   fill(120, 80, 30);
   triangle(-5, 0, 18, -18 + duck.wingOffset, 12, 5);
 
+  // eye
   fill(0);
   ellipse(-22, -14, 3, 3);
 
@@ -95,10 +112,15 @@ function drawDuck() {
 
 function drawHUD() {
   fill(0);
-  textSize(24);
+  textSize(22);
   textAlign(LEFT, TOP);
   text("Score: " + score, 20, 20);
-  text("Shots: " + shots, 20, 55);
+  text("Shots: " + shots, 20, 50);
+  text("Misses: " + misses, 20, 80);
+  text("Escaped: " + escaped, 20, 110);
+
+  let accuracy = shots > 0 ? round((score / shots) * 100) : 0;
+  text("Accuracy: " + accuracy + "%", 20, 140);
 
   let secondsLeft = ceil(gameTime / 60);
   textAlign(RIGHT, TOP);
@@ -123,7 +145,10 @@ function mousePressed() {
 
   if (hitDuck(mouseX, mouseY)) {
     score++;
+    hitMessageTimer = 20;
     resetDuck();
+  } else {
+    misses++;
   }
 }
 
@@ -136,6 +161,15 @@ function hitDuck(px, py) {
   );
 }
 
+function drawHitMessage() {
+  if (hitMessageTimer > 0) {
+    fill(255, 215, 0);
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text("HIT!", width / 2, 50);
+  }
+}
+
 function drawGameOver() {
   fill(0, 180);
   rect(0, 0, width, height);
@@ -143,21 +177,33 @@ function drawGameOver() {
   fill(255);
   textAlign(CENTER, CENTER);
   textSize(42);
-  text("Game Over", width / 2, height / 2 - 40);
+  text("Game Over", width / 2, height / 2 - 60);
 
   textSize(28);
-  text("Final Score: " + score, width / 2, height / 2 + 5);
+  text("Final Score: " + score, width / 2, height / 2 - 10);
+  text("Ducks Escaped: " + escaped, width / 2, height / 2 + 25);
+
+  let accuracy = shots > 0 ? round((score / shots) * 100) : 0;
+  textSize(22);
+  text("Accuracy: " + accuracy + "%", width / 2, height / 2 + 60);
 
   textSize(20);
-  text("Press R to restart", width / 2, height / 2 + 45);
+  text("Press R to restart", width / 2, height / 2 + 100);
 }
 
 function keyPressed() {
   if ((key === "r" || key === "R") && gameOver) {
-    score = 0;
-    shots = 0;
-    gameTime = 60 * 30;
-    gameOver = false;
-    resetDuck();
+    restartGame();
   }
+}
+
+function restartGame() {
+  score = 0;
+  shots = 0;
+  misses = 0;
+  escaped = 0;
+  gameTime = 60 * 30;
+  gameOver = false;
+  hitMessageTimer = 0;
+  resetDuck();
 }
